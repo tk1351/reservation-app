2b9c30988a70d53e6c7ce3dd708b52d88332a092
TK1351 <takakikitsui0111@gmail.com>
2020-04-21 14:26:10 +0900

    Udemy 82 ProductModule2まで

---
+++ src/app/product/product.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'detail', component: ProductDetailComponent }
+];
+
+
+@NgModule({
+  declarations: [
+    ProductDetailComponent,
+    ProductListComponent  
+  ],
+  imports: [
+    RouterModule.forChild(routes),
+    CommonModule
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class ProductModule { }
